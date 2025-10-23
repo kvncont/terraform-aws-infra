a7ff30770a48852256afbc7636ee663a9060c7a9
Kevin Contreras Rodríguez <kvncont@gmail.com>
2025-10-23 19:09:54 +0000

    fix: comentar secciones de código relacionadas con la función Lambda y el archivo ZIP

--- lambdas/main.tf
+++ lambdas/main.tf
@@ -4,11 +4,11 @@ locals {
 }
 
 # Crear el archivo ZIP con el código de la Lambda
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  source_dir  = "${path.module}/src/"
-  output_path = "${path.module}/lambda_function.zip"
-}
+# data "archive_file" "lambda_zip" {
+#   type        = "zip"
+#   source_dir  = "${path.module}/src/"
+#   output_path = "${path.module}/lambda_function.zip"
+# }
 
 # Rol de ejecución para la Lambda
 resource "aws_iam_role" "lambda_execution_role" {
@@ -38,42 +38,42 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda_execution_role.name
 }
 
-# Función Lambda
-resource "aws_lambda_function" "main" {
-  depends_on = [
-    aws_iam_role_policy_attachment.lambda_basic_execution,
-    data.archive_file.lambda_zip
-  ]
+# # Función Lambda
+# resource "aws_lambda_function" "main" {
+#   depends_on = [
+#     aws_iam_role_policy_attachment.lambda_basic_execution,
+#     data.archive_file.lambda_zip
+#   ]
 
-  function_name = var.lambda_function_name
-  role          = aws_iam_role.lambda_execution_role.arn
-  handler       = var.lambda_handler
-  runtime       = var.lambda_runtime
-  timeout       = var.lambda_timeout
-  memory_size   = var.lambda_memory_size
-  description   = var.lambda_description
+#   function_name = var.lambda_function_name
+#   role          = aws_iam_role.lambda_execution_role.arn
+#   handler       = var.lambda_handler
+#   runtime       = var.lambda_runtime
+#   timeout       = var.lambda_timeout
+#   memory_size   = var.lambda_memory_size
+#   description   = var.lambda_description
 
-  filename         = data.archive_file.lambda_zip.output_path
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+#   filename         = data.archive_file.lambda_zip.output_path
+#   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
-  dynamic "environment" {
-    for_each = length(var.lambda_environment_variables) > 0 ? [1] : []
-    content {
-      variables = var.lambda_environment_variables
-    }
-  }
+#   dynamic "environment" {
+#     for_each = length(var.lambda_environment_variables) > 0 ? [1] : []
+#     content {
+#       variables = var.lambda_environment_variables
+#     }
+#   }
 
-  tags = {
-    Name = var.lambda_function_name
-  }
-}
+#   tags = {
+#     Name = var.lambda_function_name
+#   }
+# }
 
 # CloudWatch Log Group para la Lambda
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  name              = "/aws/lambda/${var.lambda_function_name}"
-  retention_in_days = 7
+# resource "aws_cloudwatch_log_group" "lambda_logs" {
+#   name              = "/aws/lambda/${var.lambda_function_name}"
+#   retention_in_days = 7
 
-  tags = {
-    Name = "${var.lambda_function_name}-logs"
-  }
-}
+#   tags = {
+#     Name = "${var.lambda_function_name}-logs"
+#   }
+# }
